701c46f0e0277eafb6c9b18b8dfc5e5eac28f006
ChristianDaniel <christian_daniel04@hotmail.com>
2022-01-22 15:00:24 -0300

    page Home principal

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,26 @@
+import React from 'react'
+
 import type { NextPage } from 'next'
+import dynamic from 'next/dynamic'
+
+import Head from 'next/head'
+
+const Header = dynamic(() => import('../src/components/Header'), {
+  ssr: false
+})
+
+const HomeContainer = dynamic(() => import('../src/features/HomeContainer'), {
+  ssr: false
+})
 
 const Home: NextPage = () => {
   return (
     <>
-      <h1>hello World</h1>
+      <Head>
+        <title>Portifolio</title>
+      </Head>
+      <Header />
+      <HomeContainer />
     </>
   )
 }
