0511b1198e64ded8afbc419f7a336b60f9a4c797
ChristianDaniel <christian_daniel04@hotmail.com>
2022-01-21 22:40:06 -0300

    create component Button

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,12 @@
+import { ButtonHTMLAttributes } from 'react'
+import React from 'react'
+
+import { Buttons } from './styles'
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>
+
+const Button = ({ ...props }: ButtonProps): JSX.Element => {
+  return <Buttons {...props} />
+}
+
+export default Button
