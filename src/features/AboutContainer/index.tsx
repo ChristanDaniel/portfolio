6de2384144c47c39ccaaf94a2bafee624d038c87
ChristianDaniel <christian_daniel04@hotmail.com>
2022-01-23 14:54:22 -0300

    create feature AboutContainer

---
+++ src/features/AboutContainer/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import { FiChevronsRight } from 'react-icons/fi'
+
+import { MainContainer } from './styles'
+
+const AboutContainer = (): JSX.Element => {
+  return (
+    <>
+      <MainContainer></MainContainer>
+    </>
+  )
+}
+
+export default AboutContainer
---
+++ src/features/AboutContainer/styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+const MainContainer = styled.main`
+  background: #232323;
+  position: relative;
+`
+
+export { MainContainer }
