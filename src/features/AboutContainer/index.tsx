3f5a99de787ad91e0529ad36b00959936550ba03
ChristianDaniel <christian_daniel04@hotmail.com>
2022-01-23 14:54:49 -0300

    estrutura feature AboutContainer

--- src/features/AboutContainer/index.tsx
+++ src/features/AboutContainer/index.tsx
@@ -7,7 +7,25 @@ import { MainContainer } from './styles'
 const AboutContainer = (): JSX.Element => {
   return (
     <>
-      <MainContainer></MainContainer>
+      <MainContainer>
+        <TitleContainer>
+          <FiChevronsRight /> Sobre mim
+        </TitleContainer>
+        <AboutContent>
+          <div>
+            <img src="https://github.com/ChristanDaniel.png" alt="My-Image" />
+          </div>
+
+          <DescriptionAbout>
+            <p>
+              Meu Nome é <span>Christian Daniel</span>, tenho 23 anos, moro em Dourados-MS.
+            </p>
+            <p>tenho 6 anos de experiência com atendimento ao público.</p>
+            <p>estudei 4 semestre da faculdade de Matemática, eu sai no final de 2020</p>
+            <p>para me dedicar a carreira de desenvolvedor Front-end.</p>
+          </DescriptionAbout>
+        </AboutContent>
+      </MainContainer>
     </>
   )
 }
