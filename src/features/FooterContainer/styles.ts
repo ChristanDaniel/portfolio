d6e4c72cdf31f986241a1c16cb740bfe70185f39
ChristianDaniel <christian_daniel04@hotmail.com>
2022-01-24 21:57:59 -0300

    css feature footerContainer

---
+++ src/features/FooterContainer/styles.ts
@@ -0,0 +1,23 @@
+import styled from 'styled-components'
+
+const Footer = styled.footer`
+  background: #232323;
+  width: 100%;
+  height: 100px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  p {
+    color: #616480;
+    margin-bottom: 12px;
+    font-size: smaller;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: underline;
+  }
+`
+
+export { Footer }
