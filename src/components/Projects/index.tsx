b8fc4b162a8b335773695a5562b0fe378deba2c3
ChristianDaniel <christian_daniel04@hotmail.com>
2022-01-25 21:44:33 -0300

    create component Projects

---
+++ src/components/Projects/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import {} from './styles'
+
+interface ProjectProps {
+  name: string
+  description: string
+  FirstImg: string
+  SecondImg?: string
+  githubLink: string
+  siteLink: string
+}
+
+const Projects = ({ name, description, FirstImg, SecondImg, githubLink, siteLink }: ProjectProps): JSX.Element => {
+  return <></>
+}
+
+export default Projects
