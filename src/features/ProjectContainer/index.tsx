9f887548025677b95ce17e47b58f43b198d197da
ChristianDaniel <christian_daniel04@hotmail.com>
2022-01-27 21:28:24 -0300

    create feature ProjectContainer

---
+++ src/features/ProjectContainer/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { MainContainer } from './styles'
+
+const ProjectContainer = (): JSX.Element => {
+  return (
+    <>
+      <MainContainer></MainContainer>
+    </>
+  )
+}
+
+export default ProjectContainer
---
+++ src/features/ProjectContainer/styles.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+const MainContainer = styled.main`
+  position: relative;
+`
+
+export { MainContainer }
