cb0f6842530381eb4cba19a0a3692f5b90548638
ChristianDaniel <christian_daniel04@hotmail.com>
2022-01-30 15:25:41 -0300

    css button header

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -19,6 +19,31 @@ const NameContent = styled.h2`
   margin-left: 90px;
 `
 
+const LiContent = styled.li`
+  height: 75px;
+  display: flex;
+  position: relative;
+  align-items: center;
+
+  a {
+    padding: 0 0.5rem;
+    height: 75px;
+    line-height: 80px;
+    color: #fff;
+
+    transition: 0.3s;
+
+    &:hover {
+      color: #6a98f0;
+      font-weight: bold;
+      border-bottom: 3px solid #6a98f0;
+      border-bottom-left-radius: 3px;
+      border-bottom-right-radius: 3px;
+      width: 100%;
+    }
+  }
+`
+
 const NavigationItens = styled.ul`
   margin-right: 170px;
   display: flex;
